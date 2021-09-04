f900865cc83c2b9ac1091278352a4ee57e82ea98
felor <sergey.a.fedorchenko@gmail.com>
2021-09-04 17:09:15 +0000

    better logging and model saving

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ from torch.utils.data import DataLoader
 from tqdm import tqdm
 import model
 import utils
+import numpy as np
 
 # Define training data loader
 train_dataset = utils.G2NetDataSet(main_folder='/slowfs/datasets/g2net-gravitational-wave-detection',
@@ -18,8 +19,9 @@ criterion = torch.nn.BCELoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=0.0001)
 
 # Training
-for epoch in range(1):
+for epoch in range(50):
     progress_bar = tqdm(train_loader)
+    loss_l = []
     for data in progress_bar:
         # get the inputs; data is a list of [inputs, labels]
         inputs, labels, _ = data
@@ -32,10 +34,12 @@ for epoch in range(1):
         loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
-
+        loss_l += [loss.item()]
         # Print statistics
-        progress_bar.set_postfix({'loss': loss.item()})
+        progress_bar.set_postfix({'loss': np.mean(loss_l)})
+
+    torch.save(model, "models/logit_v2_"+str(epoch)+".pt")
 
 print('Finished Training')
-torch.save(model, "models/logit_v1.pt")
+torch.save(model, "models/logit_v2.pt")
 print('Saved the trained model into logit_v1.pt')
