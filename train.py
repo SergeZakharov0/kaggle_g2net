d074aae7b0394f9a29ba62f69df7a5c77ca8e702
Sergey Zakharov <serge.zakharov@mail.ru>
2021-09-06 09:09:36 +0000

    Refactor train.py a little bit

--- train.py
+++ train.py
@@ -10,11 +10,11 @@ train_dataset = utils.G2NetDataSet(main_folder='/slowfs/datasets/g2net-gravitati
                                    set_type='train',
                                    labels_file='training_labels.csv', subset_ind=range(20000))
 train_loader = DataLoader(train_dataset, batch_size=64)
-
-test_dataset = utils.G2NetDataSet(main_folder='/slowfs/datasets/g2net-gravitational-wave-detection',
+# Define validation data loader
+val_dataset = utils.G2NetDataSet(main_folder='/slowfs/datasets/g2net-gravitational-wave-detection',
                                    set_type='train',
                                    labels_file='training_labels.csv', subset_ind=range(20000, 22000))
-test_loader = DataLoader(test_dataset, batch_size=64)
+val_loader = DataLoader(val_dataset, batch_size=64)
 
 # Define model
 model = model.Model()
@@ -31,27 +31,25 @@ for epoch in range(1, 50):
     for data in progress_bar:
         # get the inputs; data is a list of [inputs, labels]
         inputs, labels, _ = data
-
         # zero the parameter gradients
         optimizer.zero_grad()
-
         # forward + backward + optimize
         outputs = model(inputs)
         loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
         loss_l += [loss.item()]
-
         # Print statistics
         progress_bar.set_postfix({'loss': np.mean(loss_l)})
-    progress_bar = tqdm(test_loader)
+    # Validate the epoch
+    progress_bar = tqdm(val_loader)
     for data in progress_bar:
         inputs, labels, filename = data
         outputs = model(inputs)
         outputs = (outputs > 0.5).float()
         acc = (outputs == labels).float().sum()/len(outputs)
         progress_bar.set_postfix({'acc': float(acc)})
-
+    # Save the model after every fifth
     if epoch % 5 == 0:
         torch.save(model, "models/logit_v3_"+str(epoch)+".pt")
 
