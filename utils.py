a71be944568655625d207c51ee4291276ac0d5d0
Sergey Zakharov <serge.zakharov@mail.ru>
2021-09-06 09:04:53 +0000

    Add std and mean parameters to G2NetDataSet

--- utils.py
+++ utils.py
@@ -7,6 +7,7 @@ import pandas as pd
 from nnAudio.Spectrogram import CQT1992v2
 from scipy.stats import shapiro
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 # Get CQT transform, create a function for data preprocessing
 transform = CQT1992v2(sr=2048, fmin=22, fmax=1024,
@@ -29,14 +30,9 @@ def wave2spectrogram(raw_data):
     return torch.from_numpy(prep_res)
 
 
-def gray_filtering(unfiltered_data, filter_func=np.min):
-    # Noise filtering. Rounds everything to the colours of gray
-    min_color_data = filter_func(unfiltered_data, axis=2, keepdims=True)
-    return np.concatenate((min_color_data, min_color_data, min_color_data), axis=2)
-
-
 class G2NetDataSet(Dataset):
-    def __init__(self, main_folder, set_type, labels_file=None):
+    def __init__(self, main_folder, set_type, labels_file=None, 
+                 subset_ind=None, mean=None, std=None):
         Dataset.__init__(self)
         self.main_folder = main_folder
         self.set_type = set_type
@@ -51,25 +47,13 @@ class G2NetDataSet(Dataset):
             self.labels_list = pd.read_csv(os.path.join(main_folder, labels_file))
         else:
             self.labels_list = None
-
-        self.files_list = self.files_list[:500]
-
-        self.mean = None
-        mean_sq = None
-        for a, _, _ in self:
-            if self.mean is None:
-                self.mean = a
-                mean_sq = torch.pow(a, 2)
-            else:
-                self.mean += a
-                mean_sq += torch.pow(a, 2)
-
-        if self.mean is not None:
-            self.mean /= len(self)
-            self.std = torch.pow((mean_sq / len(self)- torch.pow(self.mean, 2)), 0.5)
-
-        self.transform = transforms.Compose([transforms.Normalize(self.mean, self.std)])
-
+        if subset_ind is not None:
+            self.files_list = np.array(self.files_list)[subset_ind]
+        # Create a transform for data normalization if necessary
+        if mean is not None and std is not None:
+            self.mean = mean
+            self.std = std
+            self.transform = transforms.Compose([transforms.Normalize(self.mean, self.std)])
 
     def __len__(self):
         return len(self.files_list)
@@ -81,7 +65,7 @@ class G2NetDataSet(Dataset):
             label = self.labels_list[self.labels_list.id == filename_short].to_numpy()[0, 1]
         else:
             label = None
-
+        # Transform data
         data = wave2spectrogram(np.load(self.files_list[idx]))
         if self.transform:
             data = self.transform(data)
