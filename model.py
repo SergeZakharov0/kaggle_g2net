069b555c0831b2c79adf1701e9aa872908d644d6
felor <sergey.a.fedorchenko@gmail.com>
2021-09-07 16:32:48 +0000

    calculation of train tensors mean and std

--- model.py
+++ model.py
@@ -5,11 +5,13 @@ import torch
 class Model(torch.nn.Module):
     def __init__(self):
         super(Model, self).__init__()
-        self.fc1 = torch.nn.Linear(128 * 128 * 3, 128 * 128)
-        self.fc3 = torch.nn.Linear(128 * 128, 1)
+        self.fc1 = torch.nn.Linear(128 * 128 * 3, 128 * 32)
+        self.fc2 = torch.nn.Linear(32 * 128, 128)
+        self.fc3 = torch.nn.Linear(128, 1)
 
     def forward(self, x):
         x = torch.flatten(x, 1)
         x = self.fc1(x)
+        x = self.fc2(x)
         x = torch.sigmoid(self.fc3(x))
         return x.double()
--- train.py
+++ train.py
@@ -5,24 +5,28 @@ import model
 import utils
 import numpy as np
 
+mean = torch.load('mean')
+std = torch.load('std')
 # Define training data loader
 train_dataset = utils.G2NetDataSet(main_folder='/slowfs/datasets/g2net-gravitational-wave-detection',
                                    set_type='train',
-                                   labels_file='training_labels.csv', subset_ind=range(20000))
+                                   labels_file='training_labels.csv', subset_ind=range(20000),
+                                   mean=mean, std=std)
 train_loader = DataLoader(train_dataset, batch_size=64)
 # Define validation data loader
 val_dataset = utils.G2NetDataSet(main_folder='/slowfs/datasets/g2net-gravitational-wave-detection',
                                    set_type='train',
-                                   labels_file='training_labels.csv', subset_ind=range(20000, 22000))
+                                   labels_file='training_labels.csv', subset_ind=range(20000, 25000),
+                                   mean=mean, std=std)
 val_loader = DataLoader(val_dataset, batch_size=64)
 
-# Define model
+# Define models
 model = model.Model()
 model.zero_grad()
 
 # Define loss and optimizer
 criterion = torch.nn.BCELoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=0.001)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.0001)
 
 # Training
 for epoch in range(1, 50):
@@ -43,13 +47,15 @@ for epoch in range(1, 50):
         progress_bar.set_postfix({'loss': np.mean(loss_l)})
     # Validate the epoch
     progress_bar = tqdm(val_loader)
+
+    acc = []
     for data in progress_bar:
         inputs, labels, filename = data
         outputs = model(inputs)
         outputs = (outputs > 0.5).float()
-        acc = (outputs == labels).float().sum()/len(outputs)
-        progress_bar.set_postfix({'acc': float(acc)})
-    # Save the model after every fifth
+        acc += [(outputs == labels).float().sum()/len(outputs)]
+        progress_bar.set_postfix({'acc': float(np.mean(acc))})
+    # Save the models after every fifth
     if epoch % 5 == 0:
         torch.save(model, "models/logit_v3_"+str(epoch)+".pt")
 
